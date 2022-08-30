abaf5beb397395729f36f69cd8b63fce05f5988a
bureson2@fel.cvut.cz <bureson2@fel.cvut.cz>
2022-08-30 13:26:56 +0200

    ...

--- soloproject/src/test/java/bootcamp/soloproject/service/MonitoringResultServiceTest.java
+++ soloproject/src/test/java/bootcamp/soloproject/service/MonitoringResultServiceTest.java
@@ -6,13 +6,17 @@ import bootcamp.soloproject.model.MonitoringResult;
 import bootcamp.soloproject.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 class MonitoringResultServiceTest {
 
